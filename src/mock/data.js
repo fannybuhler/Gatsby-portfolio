5c2b19fd832b471f1c0aea56a97f1c39e180fe70
fannybuhler <fanny.buhler1@gmail.com>
2022-04-28 14:53:17 +0200

    Move content to data-file

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,17 +9,17 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, my name is',
+  name: 'Fanny Buhlér',
+  subtitle: "I'm a frontend developer at TietoEvry.",
+  cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'Me_round.png',
-  paragraphOne: `I'm an eager-to-learn tech and denim enthusiast studying to become a frontend developer. I'm often described as hard working and service minded with a positive attitude, and I leave no stone unturned when it comes to solving problems.`,
-  paragraphTwo: `I've got 27 weeks of internship coming up starting in September 2021, and I'm looking to find a team that can help me expand my creativity and curiosity, and to become a better coder.`,
+  paragraphOne: `I'm a junior developer and am often described as service minded with a positive attitude. Lately I've mainly been working with Vue.js, but I'm always up for a creative project to take on.`,
+  paragraphTwo: `If you want to you can have a look at my resume below.`,
   paragraphThree: '',
   resume: '/CV.pdf', // if no resume, the button will not show up
 };
@@ -30,7 +30,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'braive_snap.png',
     title: 'Braive',
-    info: 'Braive offers mental health checks and uses evidence based psychology to help you tackle challenges and train your mental skills. I helped them with UX and design for their logged in users, creating a new dashboard showcasing their active programs and progress. The project was delivered in Figma with solutions for both desktop and mobile devices.',
+    info:
+      'Braive offers mental health checks and uses evidence based psychology to help you tackle challenges and train your mental skills. I helped them with UX and design for their logged in users, creating a new dashboard showcasing their active programs and progress. The project was delivered in Figma with solutions for both desktop and mobile devices.',
     info2: 'Figma, Design, UX',
     url: 'https://braive.com/',
     repo: '', // if no repo, the button will not show up
@@ -39,7 +40,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'ricola_snap.png',
     title: 'Ricola',
-    info: 'Ricola is a pioneer in organic herb cultivation and famous for their original Swiss herb drops. This project was a proposal of how Ricola can tell the world about their extensive climate efforts and their goal of going beyond zero-emissions.',
+    info:
+      'Ricola is a pioneer in organic herb cultivation and famous for their original Swiss herb drops. This project was a proposal of how Ricola can tell the world about their extensive climate efforts and their goal of going beyond zero-emissions.',
     info2: 'React, Gatsby JAMstack, Agile/Scrum',
     url: 'https://www.ricola.com/en-us',
     repo: '', // if no repo, the button will not show up
@@ -48,7 +50,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'frootz_snap.png',
     title: 'Frootz',
-    info: 'This was my first ever vanilla JavaScript game which I created as part of a brief at Hyper Island. The task was to create an interactive page using HTML, CSS and JS. I was to take user input - mouse or keyboard events  - and respond to said input without using frameworks or external libraries. The result was a colorful catch-the-falling-fruits-game.',
+    info:
+      'This was my first ever vanilla JavaScript game which I created as part of a brief at Hyper Island. The task was to create an interactive page using HTML, CSS and JS. I was to take user input - mouse or keyboard events  - and respond to said input without using frameworks or external libraries. The result was a colorful catch-the-falling-fruits-game.',
     info2: 'Concept, Design, HTML, CSS, JavaScript',
     url: 'https://fannybuhler.github.io/first-JS-game/',
     repo: 'https://github.com/Fannybuhler/first-JS-game', // if no repo, the button will not show up
